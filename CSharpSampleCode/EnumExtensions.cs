35dd133f7a4ee2d620f0d155f235d77db1b541f2
Philip Miller <pwm1234-sri@users.noreply.github.com>
2024-06-27 09:59:31 -0400

    update GetDisplayName to handle invalid values

--- CSharpSampleCode/EnumExtensions.cs
+++ CSharpSampleCode/EnumExtensions.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Reflection;
@@ -9,17 +9,31 @@ namespace CSharpSampleCode
     {
         public static string GetDisplayName(this Enum enumValue)
         {
-            // A much heavier weight approach would be to use the Humanizer package
-            // [Humanize Enums]
+            // The initial implementation came from 
+            // https://dnilvincent.net/blog/posts/how-to-get-enum-display-name-in-csharp-net
+
+            // A much more heavy weight and elegant approach would be to use
+            // the Humanizer package [Humanize Enums]
             // (https://github.com/Humanizr/Humanizer?tab=readme-ov-file#humanize-enums)
+            // I am also unsure how it would handle the invalid value case.
 
             string valStr = enumValue.ToString();
-            var displayAttribute = enumValue.GetType()
-                .GetMember(valStr)
-                .First()
-                .GetCustomAttribute<DisplayAttribute>();
-            string displayName =
-                displayAttribute?.GetName() ?? valStr;
+            string displayName;
+            try
+            {
+                MemberInfo[] memberInfos = enumValue.GetType().GetMember(valStr);
+                MemberInfo memberInfo = memberInfos.First();
+                var displayAttr =
+                    memberInfo?.GetCustomAttribute<DisplayAttribute>();
+                displayName = displayAttr?.GetName() ?? valStr;
+            }
+            catch (System.InvalidOperationException)
+            {
+                // get here when an int value that is not one of the enum
+                // values is cast to an enum.
+                displayName = $"INVALID({valStr})";
+            }
+
             return displayName;
         }
     }
